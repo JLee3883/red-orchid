4c8512cd580f30e39578c589f126b051951dae52
Paper Girl <jamie.rose.528@gmail.com>
2022-01-28 22:56:18 -0800

    user model, flowtalk model, daily info model done

---
+++ server/models/dailyInfo.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const dailyInfoSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+    required: true,
+  },
+  flow: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: false,
+  },
+  pain: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: false,
+  },
+  mood: {
+    type: String,
+    required: false,
+  },
+  cravings: {
+    type: [],
+    required: false,
+  },
+  notes: {
+    type: String,
+    minlength: 1,
+    maxlength: 280,
+  },
+});
+
+const Tech = model("Tech", techSchema);
+
+module.exports = Tech;
