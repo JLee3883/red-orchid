9cb65a79052ea048f3156b71c5e422d6e6f18c81
JLee <thishereisjenn@gmail.com>
2022-01-31 11:44:25 -0800

    altered queries and mutations, but still need some more altering

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,31 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const CREATE_FLOW_TITLE = gql`
+  mutation createFlowTitle($tech1: String!, $tech2: String!) {
     createMatchup(tech1: $tech1, tech2: $tech2) {
       _id
       tech1
@@ -10,14 +34,26 @@ export const CREATE_MATCHUP = gql`
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
+export const ADD_COMMENT = gql`
+  mutation addComment(
+    $thoughtId: ID!
+    $commentText: String!
+    $commentAuthor: String!
+  ) {
+    addComment(
+      thoughtId: $thoughtId
+      commentText: $commentText
+      commentAuthor: $commentAuthor
+    ) {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
     }
   }
 `;
