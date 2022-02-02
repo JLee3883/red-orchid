b088e5bcd63b7f14556f9a8a71722d9e0756ea56
Paper Girl <jamie.rose.528@gmail.com>
2022-02-02 11:25:16 -0800

    navbar

---
+++ client/src/components/navbar/NavBar.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import "./NavBar.css"
+// import {Navbar} from "react-bootstrap"
+
+// const NavBar = () => {
+//   return  <Navbar bg="dark" variant="dark">
+//   <Container>
+//     <Navbar.Brand href="#home">
+//       <img
+//         alt=""
+//         src="/logo.svg"
+//         width="30"
+//         height="30"
+//         className="d-inline-block align-top"
+//       />{' '}
+//     React Bootstrap
+//     </Navbar.Brand>;
+// };
+
+// export default NavBar;
