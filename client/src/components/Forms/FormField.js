f40bacff745a7ebd8ba491d533ab5956fe84db49
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2022-02-02 22:16:25 -0800

    add more style

--- client/src/components/Forms/FormField.js
+++ client/src/components/Forms/FormField.js
@@ -1,6 +1,4 @@
 import React, { useState }  from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
 import './FormField.css'
 
 const FormField = ({isMultiLine, label, change}) => {
